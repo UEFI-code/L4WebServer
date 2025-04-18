8701f3ccf7e25ac9f76ae1c95ababf6bfb634763
UEFI-code <zhuoshizhang@hotmail.com>
2025-04-18 20:23:45 +0900

    Improve Feature

--- http_processor.c
+++ http_processor.c
@@ -1,7 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "toolgets.h"
 
-char http_header_template[] = "HTTP/1.1 200 OK\r\n"
+char http_header_template_200[] = "HTTP/1.1 200 OK\r\n"
+                        "Content-Type: text/html\r\n"
+                        "Content-Length: %d\r\n"
+                        "Connection: close\r\n"
+                        "\r\n";
+char http_header_template_404[] = "HTTP/1.1 404 Not Found\r\n"
                         "Content-Type: text/html\r\n"
                         "Content-Length: %d\r\n"
                         "Connection: close\r\n"
@@ -15,10 +20,18 @@ void reply_http_req(struct sockaddr_in *guestAddr, char *http_req, uint32_t req_
     fwrite(http_req, 1, req_len, stdout);
     printf("\n----------------------------------\n");
 
+    if (strncmp(http_req, "GET / ", 6) != 0) {
+        // 404 Not Found
+        sprintf_s(http_res, TX_PayloadSize, http_header_template_404, 4);
+        strcat_s(http_res, TX_PayloadSize, "404");
+        *res_len = strlen(http_res) + 1;
+        return;
+    }
+    
     char buf[512] = { 0 };
     uint32_t content_len = sprintf_s(buf, sizeof(buf), html_template, guestIP[0], guestIP[1], guestIP[2], guestIP[3], ntohs(guestAddr->sin_port));
 
-    sprintf_s(http_res, TX_PayloadSize, http_header_template, content_len);
+    sprintf_s(http_res, TX_PayloadSize, http_header_template_200, content_len);
     strcat_s(http_res, TX_PayloadSize, buf);
     
     *res_len = strlen(http_res) + 1;
