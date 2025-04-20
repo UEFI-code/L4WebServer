ff35c0b3327000f3164e2fd4a8ff41bffec670a2
UEFI-code <zhuoshizhang@hotmail.com>
2025-04-20 13:53:42 +0900

    Improve Code Logic

--- http_processor.c
+++ http_processor.c
@@ -20,19 +20,21 @@ void reply_http_req(struct sockaddr_in *guestAddr, char *http_req, uint32_t req_
     fwrite(http_req, 1, req_len, stdout);
     printf("\n----------------------------------\n");
 
-    if (strncmp(http_req, "GET / ", 6) != 0) {
-        // 404 Not Found
-        sprintf_s(http_res, TX_PayloadSize, http_header_template_404, 4);
-        strcat_s(http_res, TX_PayloadSize, "404");
+    // Dispatch the HTTP request
+    if (strncmp(http_req, "GET / ", 6) == 0) {
+        char html_buf[512] = { 0 };
+        uint32_t html_len = sprintf_s(html_buf, sizeof(html_buf), html_template, guestIP[0], guestIP[1], guestIP[2], guestIP[3], ntohs(guestAddr->sin_port));
+
+        sprintf_s(http_res, TX_PayloadSize, http_header_template_200, html_len);
+        strcat_s(http_res, TX_PayloadSize, html_buf);
+        
         *res_len = strlen(http_res) + 1;
         return;
     }
-    
-    char buf[512] = { 0 };
-    uint32_t content_len = sprintf_s(buf, sizeof(buf), html_template, guestIP[0], guestIP[1], guestIP[2], guestIP[3], ntohs(guestAddr->sin_port));
 
-    sprintf_s(http_res, TX_PayloadSize, http_header_template_200, content_len);
-    strcat_s(http_res, TX_PayloadSize, buf);
-    
+    // 404 Not Found
+    sprintf_s(http_res, TX_PayloadSize, http_header_template_404, 4);
+    strcat_s(http_res, TX_PayloadSize, "404");
     *res_len = strlen(http_res) + 1;
+    return;
 }
