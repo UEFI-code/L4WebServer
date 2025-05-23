986c4a550e6b23204ca567042051bd0aa8dfab33
UEFI-code <zhuoshizhang@hotmail.com>
2025-05-23 16:18:45 +0900

    Improve Code

--- http_processor.c
+++ http_processor.c
@@ -1,40 +1,39 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "toolgets.h"
 
-char http_header_template_200[] = "HTTP/1.1 200 OK\r\n"
+const char http_header_template_200[] = "HTTP/1.1 200 OK\r\n"
                         "Content-Type: text/html\r\n"
                         "Content-Length: %d\r\n"
                         "Connection: close\r\n"
                         "\r\n";
-char http_header_template_404[] = "HTTP/1.1 404 Not Found\r\n"
+const char http_header_template_404[] = "HTTP/1.1 404 Not Found\r\n"
                         "Content-Type: text/html\r\n"
                         "Content-Length: %d\r\n"
                         "Connection: close\r\n"
                         "\r\n";
 
-char html_template[] = "<html><body><h1>Hello from C!</h1><br>You are %d.%d.%d.%d:%d</body></html>";
+const char html_template[] = "<html><body><h1>Hello from C!</h1><br>You are %d.%d.%d.%d:%d</body></html>";
 
-void reply_http_req(struct sockaddr_in *guestAddr, char *http_req, uint32_t req_len, char *http_res, uint32_t *res_len) {
+void reply_http_req(struct sockaddr_in *guestAddr, char *http_rx, uint32_t rx_len, char *http_tx, uint32_t *tx_len) {
     uint8_t* guestIP = (uint8_t*)&guestAddr->sin_addr.S_un.S_addr;
     printf("------ Dumping HTTP Request ------\n");
-    fwrite(http_req, 1, req_len, stdout);
+    fwrite(http_rx, 1, rx_len, stdout);
     printf("\n----------------------------------\n");
 
     // Dispatch the HTTP request
-    if (strncmp(http_req, "GET / ", 6) == 0) {
-        char html_buf[512] = { 0 };
-        uint32_t html_len = sprintf_s(html_buf, sizeof(html_buf), html_template, guestIP[0], guestIP[1], guestIP[2], guestIP[3], ntohs(guestAddr->sin_port));
-
-        sprintf_s(http_res, TX_PayloadSize, http_header_template_200, html_len);
-        strcat_s(http_res, TX_PayloadSize, html_buf);
+    if (strncmp(http_rx, "GET / ", 6) == 0) {
+        // http_rx buffer is useless now, we can use it as scratch space
+        uint32_t html_len = sprintf_s(http_rx, RX_PayloadSize, html_template, guestIP[0], guestIP[1], guestIP[2], guestIP[3], ntohs(guestAddr->sin_port));
+        sprintf_s(http_tx, TX_PayloadSize, http_header_template_200, html_len);
+        strcat_s(http_tx, TX_PayloadSize, http_rx);
         
-        *res_len = strlen(http_res) + 1;
+        *tx_len = strlen(http_tx) + 1;
         return;
     }
 
     // 404 Not Found
-    sprintf_s(http_res, TX_PayloadSize, http_header_template_404, 4);
-    strcat_s(http_res, TX_PayloadSize, "404");
-    *res_len = strlen(http_res) + 1;
+    sprintf_s(http_tx, TX_PayloadSize, http_header_template_404, 4);
+    strcat_s(http_tx, TX_PayloadSize, "404");
+    *tx_len = strlen(http_tx) + 1;
     return;
 }
